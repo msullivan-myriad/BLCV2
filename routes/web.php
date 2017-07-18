2a87e9a921e3f0ac373ec2f9ce6cdd2fe2a7e700
Michael Sullivan <msullivan@myriadmobile.com>
2017-07-17 19:51:52 -0500

    Add pages to auth middleware group

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
@@ -22,17 +24,26 @@ Route::get('/home', 'HomeController@index');
 
 Route::get('profile', 'ProfileController@view')->name('profile');
 Route::post('profile', 'ProfileController@edit')->name('profile');
-
-
-Route::get('goals', 'GoalController@index')->name('goals');
-Route::post('goals/', 'GoalController@create')->name('create-goal');
-Route::get('goals/{goal}', 'GoalController@view')->name('view-goal');
-Route::post('goals/{goal}', 'GoalController@new')->name('new-goal');
 Route::get('search', 'GoalController@search')->name('search-goals');
 
 
+Route::group(['middleware' => ['auth']], function() {
 
-Route::get('subgoals', 'SubgoalController@index')->name('subgoals');
-Route::get('subgoals/{subgoal}', 'SubgoalController@view')->name('view-subgoal');
-Route::post('subgoals/{subgoal}/', 'SubgoalController@update')->name('update-subgoal');
-Route::delete('subgoals/{subgoal}/', 'SubgoalController@delete')->name('delete-subgoal');
+
+    Route::get('subgoals', 'SubgoalController@index')->name('subgoals');
+    Route::get('subgoals/{subgoal}', 'SubgoalController@view')->name('view-subgoal');
+    Route::post('subgoals/{subgoal}/', 'SubgoalController@update')->name('update-subgoal');
+    Route::delete('subgoals/{subgoal}/', 'SubgoalController@delete')->name('delete-subgoal');
+
+
+    Route::get('goals', 'GoalController@index')->name('goals');
+    Route::get('goals/{goal}', 'GoalController@view')->name('view-goal');
+    Route::post('goals/', 'GoalController@create')->name('create-goal');
+    Route::post('goals/{goal}', 'GoalController@new')->name('new-goal');
+
+});
+
+
+
+
+
