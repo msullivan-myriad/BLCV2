267bd9ad1b6b98bc618c2cda664b991392765cf7
Michael Sullivan <msullivan@myriadmobile.com>
2017-12-09 12:02:58 -0600

    Set up initial Collapse component and pull tags list into TagsStats

--- resources/assets/js/components/TagsStats.js
+++ resources/assets/js/components/TagsStats.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Collapse } from 'antd';
+const Panel = Collapse.Panel;
 
 class TagsStats extends Component {
 
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            tags: [],
+        }
+
+    }
+
+    componentDidMount() {
+
+        axios.get('/api/stats/users-tags')
+
+        axios.get('/api/stats/users-tags')
+            .then(response => {
+                const tags = response.data.tags;
+                this.setState({ tags });
+            });
+
     }
 
     render() {
         return (
             <div>
-                <p>This is a test section</p>
+
+                <Collapse defaultActiveKey={['1']}>
+
+                    {this.state.tags.map((tag, num) =>
+
+                        <Panel header={tag.name} key={num + 1}>
+                            <p>{tag.name}</p>
+                        </Panel>
+
+                    )}
+
+                </Collapse>
+
+
             </div>
         );
     }
