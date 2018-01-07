e9ad76b948b04729186cb085f4f138bbc3ccb2d5
Michael Sullivan <msullivan@myriadmobile.com>
2018-01-07 17:40:14 -0600

    Test that users can be created

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,18 +2,54 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class UserTest extends TestCase
-{
+class UserTest extends TestCase {
+
+    use DatabaseTransactions;
+
+    private $user;
+
+    private function createBaseUser() {
+
+      $this->user = User::create([
+          'name' => 'Jonathan',
+          'email' => 'jonathan@email.com',
+          'password' => bcrypt('password'),
+      ]);
+
+    }
 
     /** @test */
+    public function can_create_a_user() {
 
-    public function create_new_goal() {
+      $this->createBaseUser();
 
-      $this->assertTrue(true);
+      $this->assertEquals('Jonathan', $this->user->name);
+      $this->assertEquals('jonathan@email.com', $this->user->email);
+
+      //$this->assertFalse($this->user->admin);
+    }
+
+    /** @test */
+    public function can_create_an_admin_user() {
+
+      $user = User::create([
+          'name' => 'Jonathan',
+          'email' => 'jonathan@email.com',
+          'password' => bcrypt('password'),
+          'admin' => true,
+      ]);
+
+      $this->assertEquals('Jonathan', $user->name);
+      $this->assertEquals('jonathan@email.com', $user->email);
+
+      //$this->assertTrue($user->admin);
+
+
     }
 
 
