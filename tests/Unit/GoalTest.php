e13e641cd55762c377f9ac716e2e4fcf9e8341ab
Michael Sullivan <msullivan@myriadmobile.com>
2018-01-20 20:30:45 -0600

    Set up testing database and finish testing can return all goals

--- tests/Unit/GoalTest.php
+++ tests/Unit/GoalTest.php
@@ -391,16 +391,12 @@ class GoalTest extends TestCase {
     /** @test */
     public function can_return_all_goals() {
 
-      //This test is going to require that a test specific database exists
-
       factory(Goal::class, 12)->create();
 
       $findAllGoals = Goal::allGoals()->get();
 
+      $this->assertEquals(12, count($findAllGoals));
 
-      foreach ($findAllGoals as $goal) {
-        echo $goal->name;
-      }
     }
 
 }
