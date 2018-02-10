431f3c863daade58eefb6fe8def85c116172e9df
Michael Sullivan <msullivan@myriadmobile.com>
2018-02-10 11:16:22 -0600

    Create initial ProfileControllerTest

---
+++ tests/Unit/Controllers/ProfileControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\ControllerTestCase;
+
+class ProfileControllerTest extends ControllerTestCase {
+
+
+  /** @test */
+  function test_test() {
+    $this->assertTrue(true);
+  }
+
+
+}
