9d74b864180488683bd65e24796c014d8055ad89
Michael Sullivan <msullivan@myriadmobile.com>
2018-02-11 15:36:11 -0600

    Finish testing profile model

--- tests/Unit/Models/ProfileModelTest.php
+++ tests/Unit/Models/ProfileModelTest.php
@@ -22,18 +22,20 @@ class ProfileModelTest extends TestCase {
   }
 
   /** @test */
-  /*
   public function profile_can_set_dedicated_per_year() {
 
     $this->createBaseUserWithProfile();
 
-    $this->assertNull($this->user->profile->birthday);
+    $this->assertEquals($this->user->profile->cost_per_year, 0);
+    $this->assertEquals($this->user->profile->days_per_year, 0);
+    $this->assertEquals($this->user->profile->hours_per_year, 0);
 
-    $this->user->profile->setBirthday("2018-02-21");
+    $this->user->profile->setDedicatedPerYear(10, 20, 30);
 
-    $this->assertEquals($this->user->profile->birthday, '2018-02-21');
+    $this->assertEquals($this->user->profile->cost_per_year, 10);
+    $this->assertEquals($this->user->profile->days_per_year, 20);
+    $this->assertEquals($this->user->profile->hours_per_year, 30);
 
   }
-  */
 
 }
