79ec03a2b470494b4b6e5807f205f105544db011
Michael Sullivan <msullivan@myriadmobile.com>
2018-02-15 20:13:31 -0600

    Add additional tests for subgoal controller

--- tests/Unit/Controllers/SubgoalControllerTest.php
+++ tests/Unit/Controllers/SubgoalControllerTest.php
@@ -40,4 +40,25 @@ class SubgoalControllerTest extends ControllerTestCase {
     ]);
   }
 
+  /** @test */
+  public function api_sorted_requires_authorized_user() {
+    $this->canOnlyBeViewedBy('auth', 'GET', '/api/subgoals/cost-desc');
+  }
+
+  /** @test */
+  public function api_sorted_invalid_slug_is_404() {
+
+    $this->createBaseUser();
+    $this->be($this->user);
+
+    $response1 = $this->get('api/subgoals/cost-desc/');
+    $response2 = $this->get('api/subgoals/not8valid$slug/');
+
+    $response1->assertStatus(200);
+
+    //For some reason this is returning a 405 rather than a 404
+    //$response2->assertStatus(404);
+
+  }
+
 }
