51eb76215bc049ed0bcf499390db81daf7092a91
Michael Sullivan <msullivan@myriadmobile.com>
2018-02-24 16:42:15 -0600

    Beginning to test StatsController

---
+++ tests/Unit/Controllers/StatsControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\ControllerTestCase;
+
+class StatsControllerTest extends ControllerTestCase {
+
+  /** @test */
+  public function firstTest() {
+    $this->assertTrue(true);
+  }
+
+}
