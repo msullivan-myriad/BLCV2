739c7d2e03dce02f99bcda8ffe6f6b44c99a08bf
Michael Sullivan <msullivan@myriadmobile.com>
2018-03-24 22:09:20 -0500

    Cleaning up removed methods

--- resources/assets/js/components/find-goals/ListPopularGoals.js
+++ resources/assets/js/components/find-goals/ListPopularGoals.js
@@ -16,8 +16,8 @@ class ListPopularGoals extends Component {
             all_goals: [],
         }
 
-        this.loadMore = this.loadMore.bind(this);
         this.changePage = this.changePage.bind(this);
+
     }
 
     componentDidMount() {
@@ -57,31 +57,7 @@ class ListPopularGoals extends Component {
 
     }
 
-
-    loadMore() {
-
-        var url = this.state.popular_goals.next_page_url;
-
-        axios.get(url)
-
-        .then(response => {
-            let popular_goals = response.data.data.popular_goals;
-            this.setState({popular_goals})
-
-            this.setState({ all_goals: [...this.state.all_goals, ...popular_goals.data ] })
-
-        })
-
-    }
-
-
     render() {
-
-        let loadMoreBtn;
-
-        if (this.state.popular_goals.next_page_url) {
-            loadMoreBtn = <button onClick={this.loadMore}>Load More</button>;
-        }
 
         return (
             <div className="panel list-popular-goals">
