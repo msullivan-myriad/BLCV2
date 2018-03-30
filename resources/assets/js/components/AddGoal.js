5b26e0ea0423c45a67c9ef7fc16b798c82c257e7
Michael Sullivan <msullivan@myriadmobile.com>
2018-03-29 19:12:16 -0500

    Refactoring and cleaning up AddGoal component

--- resources/assets/js/components/AddGoal.js
+++ resources/assets/js/components/AddGoal.js
@@ -8,27 +8,9 @@ class AddGoal extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            id: '',
-            name: '',
-            cost: '',
-            days: '',
-            hours: '',
-            editingCost: '',
-            editingDays: '',
-            editingHours: '',
-            editing: false,
-            adding: false,
-        }
+        this.state = {}
 
-        this.costChange = this.costChange.bind(this);
-        this.daysChange = this.daysChange.bind(this);
-        this.hoursChange = this.hoursChange.bind(this);
-        this.addingGoal = this.addingGoal.bind(this);
-        this.editingGoal = this.editingGoal.bind(this);
-        this.editedGoalExit = this.editedGoalExit.bind(this);
         this.addGoal= this.addGoal.bind(this);
-        this.exitOverlay = this.exitOverlay.bind(this);
     }
 
 
@@ -40,60 +22,26 @@ class AddGoal extends Component {
             cost: this.props.goal.cost,
             days: this.props.goal.days,
             hours: this.props.goal.hours,
-            editingCost: this.props.goal.cost,
-            editingDays: this.props.goal.days,
-            editingHours: this.props.goal.hours,
-        })
-
-    }
-
-    costChange(event) {
-        this.setState({
-            editingCost: event.target.value,
         })
     }
 
-    daysChange(event) {
-        this.setState({
-            editingDays: event.target.value,
-        })
-    }
-
-    hoursChange(event) {
-        this.setState({
-            editingHours: event.target.value,
-        })
-    }
-
-    addingGoal() {
-        this.setState({
-            adding: !this.state.adding
-        })
-    }
-
-    editingGoal() {
-        this.setState({
-            adding: !this.state.adding,
-            editing: !this.state.editing
-        })
-    }
 
     addGoal() {
 
         var url = '/api/goals/';
 
         axios.post(url, {
-            cost: this.state.editingCost,
-            days: this.state.editingDays,
-            hours: this.state.editingHours,
-            goal_id: this.state.id,
+            cost: this.props.goal.cost,
+            days: this.props.goal.days,
+            hours: this.props.goal.hours,
+            goal_id: this.props.goal.id,
         })
 
         .then(response => {
 
             notification.open({
                 message: 'Success',
-                description: this.state.name + ' was added to your list',
+                description: this.props.goal.name + ' was added to your list',
                 type: 'success',
             });
 
@@ -105,7 +53,7 @@ class AddGoal extends Component {
 
                 notification.open({
                     message: 'Error',
-                    description: this.state.name + ' is already on your list',
+                    description: this.props.goal.name + ' is already on your list',
                     type: 'error',
                 });
 
@@ -122,90 +70,28 @@ class AddGoal extends Component {
 
         });
 
-        this.setState({
-            adding: false,
-            editing: false
-        })
-    }
-
-    editedGoalExit() {
-
-        this.setState({
-            editingCost: this.state.cost,
-            editingDays: this.state.days,
-            editingHours: this.state.hours,
-            adding: false,
-            editing: false,
-        })
-
-    }
-
-    exitOverlay() {
-        this.setState({
-            adding: false,
-        })
     }
 
     render() {
-
-        var inputClasses = 'default';
-        var fieldsAreDisabled = true;
-        var overlayClasses = 'overlay hidden';
-
-        if (this.state.adding) {
-            overlayClasses = 'overlay active';
-        }
-
-        if (this.state.editing) {
-            fieldsAreDisabled = false;
-            inputClasses = 'editing';
-        }
-
-
-        var rightButtonArea = <div className="right-button-area">
-                                    <button onClick={this.addingGoal}>
-                                        <i className="fa fa-plus" aria-hidden="true"></i>
-                                    </button>
-                                </div>;
-
-
-        if (this.state.editing) {
-
-            rightButtonArea = <div className="right-button-area">
-                                <button onClick={this.addGoal}>
-                                    <i className="fa fa-check" aria-hidden="true"></i>
-                                </button>
-                                <button onClick={this.editedGoalExit}>
-                                    <i className="fa fa-times" aria-hidden="true"></i>
-                                </button>
-                              </div>
-        }
-
 
         return (
             <div className="panel add-goal">
 
                 <h4>{this.props.goal.name}</h4>
-                <h5><i className="fa fa-usd" aria-hidden="true"></i> <input size={this.state.editingCost.toString().length} className={inputClasses} disabled={fieldsAreDisabled} value={this.state.editingCost} onChange={this.costChange}/></h5>
-                <h5><i className="fa fa-clock-o" aria-hidden="true"></i> <input size={this.state.editingHours.toString().length} className={inputClasses} disabled={fieldsAreDisabled} value={this.state.editingHours} onChange={this.hoursChange}/></h5>
-                <h5><i className="fa fa-calendar" aria-hidden="true"></i> <input size={this.state.editingDays.toString().length} className={inputClasses} disabled={fieldsAreDisabled} value={this.state.editingDays} onChange={this.daysChange}/></h5>
+                <h5><i className="fa fa-usd" aria-hidden="true"></i>{' ' + this.props.goal.cost}</h5>
+                <h5><i className="fa fa-clock-o" aria-hidden="true"></i> {' ' + this.props.goal.hours}</h5>
+                <h5><i className="fa fa-calendar" aria-hidden="true"></i> {' ' + this.props.goal.days} </h5>
                 <br/>
 
-                {rightButtonArea}
-
-                <div className={overlayClasses}>
+                <div className="right-button-area">
                     <button onClick={this.addGoal}>
-                        <i className="fa fa-thumbs-up" aria-hidden="true"></i>
-                        Use defaults
+                        <i className="fa fa-plus" aria-hidden="true"></i>
                     </button>
-                    <button onClick={this.editingGoal}>
-                        <i className="fa fa-pencil" aria-hidden="true"></i>
-                        Edit values
-                    </button>
-                    <button onClick={this.exitOverlay}>
-                        <i className="fa fa-times" aria-hidden="true"></i>
+                    <button onClick={ () => window.location = 'goal/' + this.props.goal.slug}>
+                        <i className="fa fa-info-circle"></i>
                     </button>
                 </div>
+
             </div>
 
         );
